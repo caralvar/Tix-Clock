9d8d05159f1b3dcad11833365a32fb29808a2d30
LuiAlv2502 <luisalvarado250205@gmail.com>
2021-10-12 17:38:42 -0600

    Added variable and prototype of input pins

--- tix_clock/tix_clock.ino
+++ tix_clock/tix_clock.ino
@@ -1,3 +1,9 @@
+const int MINUTES_BUTTON_PIN = 5;
+const int HOURS_BUTTON_PIN = 6;
+const int NUMBER_OF_INPUT_PINS = 2;
+int INPUT_PINS[] = {MINUTES_BUTTON_PIN,HOURS_BUTTON_PIN};
+void SetInputPins(int[], int[]);
+
 void setup() {
   // put your setup code here, to run once:
 
