a32543a91e8cce8d2063b9dc8b684aaef1d159a4
LuiAlv2502 <luisalvarado250205@gmail.com>
2021-10-13 15:05:47 -0600

    added the variables of the clock

--- tix_clock/tix_clock.ino
+++ tix_clock/tix_clock.ino
@@ -7,6 +7,13 @@ const int LATCH_PIN = 3;
 const int CLOCK_PIN = 4;
 const int NUMBER_OF_OUTPUT_PINS = 3;
 int OUTPUT_PINS[] = {DATA_PIN, LATCH_PIN, CLOCK_PIN};
+int horadecimas;
+int horaunidades;
+int minutosdecimas;
+int minutosunidades;
+int h = 20;
+int m = 57;
+int s = 0;
 
 void SetInputPins(int[], int);
 void SetOutputPins(int[], int);
