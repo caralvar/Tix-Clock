de528098d3d605904099b3688b2d474e0f28dc83
LuiAlv2502 <luisalvarado250205@gmail.com>
2021-10-12 19:15:03 -0600

    Correct the mistakes

--- tix_clock/tix_clock.ino
+++ tix_clock/tix_clock.ino
@@ -2,22 +2,21 @@ const int MINUTES_BUTTON_PIN = 5;
 const int HOURS_BUTTON_PIN = 6;
 const int NUMBER_OF_INPUT_PINS = 2;
 int INPUT_PINS[] = {MINUTES_BUTTON_PIN,HOURS_BUTTON_PIN};
+
 void SetInputPins(int[], int[]);
 
-void setup() {
-  // put your setup code here, to run once:
-  SetInputPins(INPUT_PINS, NUMBER_OF_INPUT_PINS);
+void setup() 
+{
+    SetInputPins(INPUT_PINS, NUMBER_OF_INPUT_PINS);
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop() 
+{
+  
 }
 void SetInputPins(int inputPins[], int numberOfInputPins)
 {
-  for (int i = 0; i < numberOfInputPins; i++)
-  {
-    pinMode(inputPins[i], INPUT);
-  }
-
+    for (int i = 0; i < numberOfInputPins; i++)
+   {
+     pinMode(inputPins[i], INPUT);
+   }
 }
