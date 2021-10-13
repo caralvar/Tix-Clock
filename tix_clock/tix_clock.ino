e24216d550fe35f92344be87a217c6471a8cca12
LuiAlv2502 <luisalvarado250205@gmail.com>
2021-10-12 19:20:58 -0600

    correct the mistake of the functions and in the prototype

--- tix_clock/tix_clock.ino
+++ tix_clock/tix_clock.ino
@@ -3,16 +3,18 @@ const int HOURS_BUTTON_PIN = 6;
 const int NUMBER_OF_INPUT_PINS = 2;
 int INPUT_PINS[] = {MINUTES_BUTTON_PIN,HOURS_BUTTON_PIN};
 
-void SetInputPins(int[], int[]);
+void SetInputPins(int[], int);
 
 void setup() 
 {
     SetInputPins(INPUT_PINS, NUMBER_OF_INPUT_PINS);
 }
+
 void loop() 
 {
   
 }
+
 void SetInputPins(int inputPins[], int numberOfInputPins)
 {
     for (int i = 0; i < numberOfInputPins; i++)
