7001f74455226bcf8d84234ea63f7353344119dd
chrisyazar28 <chrisyazar@gmail.com>
2021-10-19 19:08:02 -0600

    I added the clock operation to see it on the serial monitor and 2 variables for this functioning.

--- tix_clock/tix_clock.ino
+++ tix_clock/tix_clock.ino
@@ -1,5 +1,6 @@
 const int MINUTES_BUTTON_PIN = 5;
 const int HOURS_BUTTON_PIN = 6;
+const int MODIFY_HOUR_PIN = 7;
 const int NUMBER_OF_INPUT_PINS = 2;
 int INPUT_PINS[] = {MINUTES_BUTTON_PIN,HOURS_BUTTON_PIN};
 const int DATA_PIN = 2;
@@ -7,6 +8,7 @@ const int LATCH_PIN = 3;
 const int CLOCK_PIN = 4;
 const int NUMBER_OF_OUTPUT_PINS = 3;
 int OUTPUT_PINS[] = {DATA_PIN, LATCH_PIN, CLOCK_PIN};
+const int SECONDS_PERIOD_IN_MS = 1000;
 int hour = 0;
 int minute = 0;
 int second = 0;
@@ -27,7 +29,16 @@ void setup()
 
 void loop() 
 {
-    
+    if (digitalRead(MODIFY_HOUR_PIN))
+    {
+        hour = AddOneBasedOnInput(HOURS_BUTTON_PIN, hour);
+        minute = AddOneBasedOnInput(MINUTES_BUTTON_PIN, minute);    
+    } 
+    Serial.print("Hora: ");
+    Serial.println(hour);
+    Serial.print("Minutos: ");
+    Serial.println(minute);
+    delay(SECONDS_PERIOD_IN_MS);      
 }
   
 void SetInputPins(int inputPins[], int numberOfInputPins)
